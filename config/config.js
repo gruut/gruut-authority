c9202695d26714b76a8a6ec8b1b3c075e268e4d6
Junsuk Park <park64kr63@gmail.com>
2018-11-02 11:52:46 +0900

    ADD - Sequelize(ORM)

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.SEQUELIZE_USER,
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'gruut_authority',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  test: {
+    username: process.env.SEQUELIZE_USER,
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'gruut_authority_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  production: {
+    username: process.env.SEQUELIZE_USER,
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'gruut_authority',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+};
