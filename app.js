2d63fc32959567c40621b117addecd2e26a5606c
Junsuk Park <park64kr63@gmail.com>
2018-11-08 11:54:48 +0900

    ADD - Logger

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bugsnag = require('bugsnag');
+const accessLogger = require('morgan');
+require('winston-daily-rotate-file');
+const rfs = require('rotating-file-stream');
+const winston = require('winston');
+const fs = require('fs');
 const cert = require('./utils/cert');
 const indexRouter = require('./routes/index');
 const v1 = require('./routes/v1');
 
-bugsnag.register('135360f339967fe35d6fb8c9d47fa779');
-
 const app = express();
+
+// eslint-disable-next-line new-cap
+const transport = new (winston.transports.DailyRotateFile)({
+  filename: path.join('/tmp/log/error', 'ga-%DATE%.log'),
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+
+const errorLogger = winston.createLogger({
+  format: winston.format.combine(winston.format.json(), winston.format.timestamp()),
+  transports: [
+    transport,
+  ],
+});
 
 // set global variable
 cert.generateKeyPair();
@@ -19,21 +36,33 @@ cert.generateKeyPair();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV === 'production') {
+  const accessLogStream = rfs('access.log', {
+    interval: '1w',
+    path: path.join('/tmp/log', 'access'),
+  });
+
+  app.use(accessLogger('combined', { stream: accessLogStream }));
+} else {
+  const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+  app.use(accessLogger('combined', { stream: accessLogStream }));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bugsnag.requestHandler);
-app.use(bugsnag.errorHandler);
-
 app.use('/', indexRouter);
 app.use('/v1', v1);
 
-bugsnag.notify(new Error('Test error'));
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+  const errorMsg = `Headers: ${JSON.stringify(req.headers)} Body: ${JSON.stringify(req.body)}`;
+
+  errorLogger.log({
+    level: 'warn',
+    message: errorMsg,
+  });
   next(createError(404));
 });
 
@@ -42,6 +71,12 @@ app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  errorLogger.log({
+    level: 'error',
+    message: `Error: ${err.message}
+    Stack: ${JSON.stringify(err.stack)}`,
+  });
 
   // render the error page
   res.status(err.status || 500);
