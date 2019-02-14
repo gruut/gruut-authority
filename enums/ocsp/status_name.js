4d468b3253b7a923072d0c8e51b586e0b0a4c924
Junsuk Park <park64kr63@gmail.com>
2019-02-14 10:54:32 +0900

    # ADD - OCSP Response 상태 추가(UNKNOWN)

---
+++ enums/ocsp/status_name.js
@@ -0,0 +1,5 @@
+module.exports = {
+  GOOD: 0,
+  REVOKED: 1,
+  UNKNOWN: 2,
+};
--- utils/ocsp.js
+++ utils/ocsp.js
@@ -3,61 +3,66 @@ const Pkijs = require('pkijs');
 const Asn1js = require('asn1js');
 const WebCrypto = require('node-webcrypto-ossl');
 
+const StatusName = require('../enums/ocsp/status_name');
+const StatusValue = require('../enums/ocsp/status_value');
+
 const {
   User,
 } = require('../models');
 
 async function getOCSPResponse(ocspRequest) {
-  Pkijs.setEngine();
+  // TODO: user의 certificate의 날짜가 유효한지 확인하고, 만료되었다면 serialNum 필드를 NULL로 설정
+  setEngine();
 
   const serialNum = parseInt(ocspRequest.dReqCert.dSerialNumber.hV, 16);
+
   const user = await User.find({
     where: {
       serialNum,
     },
   });
 
-  // TODO: user의 certificate의 날짜가 유효한지 확인하고, 만료되었다면 serialNum 필드를 NULL로 설정
+  const ocspRespSimpl = new Pkijs.OCSPResponse();
+  ocspRespSimpl.responseBytes = new Pkijs.ResponseBytes();
+  ocspRespSimpl.responseBytes.responseType = '1.3.6.1.5.5.7.48.1.1'; // Basic OCSP Response
+  ocspRespSimpl.responseStatus.valueBlock.valueDec = getCertStatusValueFrom(user);
+
+  const ocspBasicResp = new Pkijs.BasicOCSPResponse();
+  ocspBasicResp.tbsResponseData.producedAt = new Date();
+
   if (user !== null) {
-    const webcrypto = new WebCrypto();
-    Pkijs.setEngine('engine', webcrypto.crypto, new Pkijs.CryptoEngine({
-      name: 'crypto_engine',
-      crypto: webcrypto,
-      subtle: webcrypto.subtle,
-    }));
-
     const pkijsCert = decodeCert(user.cert);
-    const ocspRespSimpl = new Pkijs.OCSPResponse();
-    const ocspBasicResp = new Pkijs.BasicOCSPResponse();
-
-    ocspRespSimpl.responseStatus.valueBlock.valueDec = 0; // success
-    ocspRespSimpl.responseBytes = new Pkijs.ResponseBytes();
-    ocspRespSimpl.responseBytes.responseType = '1.3.6.1.5.5.7.48.1.1'; // Basic OCSP Response
 
     ocspBasicResp.tbsResponseData.responderID = pkijsCert.issuer;
-    ocspBasicResp.tbsResponseData.producedAt = new Date();
-
-    const response = new Pkijs.SingleResponse();
-    response.certID.serialNumber.valueBlock.valueDec = serialNum;
-    response.certStatus = new Asn1js.Primitive({
-      name: 'good',
-      idBlock: {
-        tagClass: 3, // CONTEXT-SPECIFIC
-        tagNumber: 0, // [0]
-      },
-      lenBlockLength: 1, // The length contains one byte 0x00
-    }); // status - success
-    response.thisUpdate = new Date();
-
-    ocspBasicResp.tbsResponseData.responses.push(response);
-
     ocspBasicResp.certs = [pkijsCert];
 
+    // TODO: Signature 구현이 어려워서 Merger에 revocation 체크하는 기능이 추가될때 같이 구현
     // const resp = await ocspBasicResp.sign(global.keyPairs.privateKey, 'SHA-256');
-    return ocspBasicResp;
   }
 
-  return null;
+  const response = new Pkijs.SingleResponse();
+  response.certID.serialNumber.valueBlock.valueDec = serialNum;
+  response.thisUpdate = new Date();
+
+  response.certStatus = new Asn1js.Primitive({
+    idBlock: {
+      tagClass: 3, // CONTEXT-SPECIFIC
+      tagNumber: getCertStatusNameFrom(user),
+    },
+    lenBlockLength: 1, // The length contains one byte 0x00
+  });
+  ocspBasicResp.tbsResponseData.responses.push(response);
+
+  return ocspBasicResp;
+}
+
+function setEngine() {
+  const webcrypto = new WebCrypto();
+  Pkijs.setEngine('engine', webcrypto.crypto, new Pkijs.CryptoEngine({
+    name: 'crypto_engine',
+    crypto: webcrypto,
+    subtle: webcrypto.subtle,
+  }));
 }
 
 function decodeCert(pem) {
@@ -79,6 +84,23 @@ function decodeCert(pem) {
   return new Pkijs.Certificate({
     schema: asn1.result,
   });
+}
+
+function getCertStatusValueFrom(user) {
+  if (user !== null) {
+    return StatusValue.SUCCESSFUL;
+  }
+
+  return StatusValue.NOT_USED;
+}
+
+function getCertStatusNameFrom(user) {
+  if (user !== null) {
+    // TODO: Check valid
+    return StatusName.GOOD;
+  }
+
+  return StatusName.UNKNOWN;
 }
 
 module.exports = {
